b1cb2adbd58d9578928cd94cd6a080f3f834e280
es1aT <136983924+es1aT@users.noreply.github.com>
2024-05-29 00:29:45 +0900

    Update methods.py

--- methods.py
+++ methods.py
@@ -1,7 +1,7 @@
 def convert(num, n, m): # n進数をm進数に変換する（全て数字で入力、2~10まで）
     decimal = int(str(num), n) # 10進数に変換
     if decimal == 0:
-        return "0" # 元が0なら"0"を返す
+        return 0 # 元が0なら0を返す
     else:
         digits = []
         while decimal > 0:
