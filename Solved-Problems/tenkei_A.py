ad57ae8667cf7d44ad18079038545e8f2df84176
es1aT <136983924+es1aT@users.noreply.github.com>
2024-08-26 13:46:31 +0900

    Update tenkei_A.py

--- Solved-Problems/tenkei_A.py
+++ Solved-Problems/tenkei_A.py
@@ -1,4 +1,4 @@
-N, K = map(int, input().split())
+N, K = map(int, input().split()) # 典型的な二分探索問題
 A = list(map(int, input().split()))
 left, right = 0, N-1
 while left <= right:
@@ -12,5 +12,3 @@ if left == N:
   print(-1)
 else:
   print(left)
-
-# 典型的な二分探索問題
