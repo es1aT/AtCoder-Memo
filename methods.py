ccd35faae9a69c84aa62483b8236461a361ef11b
es1aT <136983924+es1aT@users.noreply.github.com>
2024-08-27 03:49:12 +0900

    Update methods.py

--- methods.py
+++ methods.py
@@ -1,9 +1,9 @@
 def convert(num, n, m):  # n進数をm進数に変換
     decimal = int(str(num), n)  # 10進数に変換
     if decimal == 0:
-        return 0
+        return "0"
     result = ""
     while decimal > 0:
         result = str(decimal % m) + result
         decimal //= m
-    return int(result)
+    return result
