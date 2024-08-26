6b27f3743eab60f228b1632bc2d30d5bc83bebaa
es1aT <136983924+es1aT@users.noreply.github.com>
2024-08-27 04:27:27 +0900

    Update tenkei90_022.py

--- Solved-Problems/tenkei90_022.py
+++ Solved-Problems/tenkei90_022.py
@@ -1,5 +1,17 @@
 # ユークリッドの互除法もしくはgcd関数で、三つの値の最大公約数を算出する。
+
+# mathのgcd関数を用いた場合
 import math
 A, B, C = map(int, input().split())
 g = math.gcd(math.gcd(A, B), C)
+print(A // g + B // g + C // g - 3)
+
+
+#ユークリッドの互除法を用いた場合
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+A, B, C = map(int, input().split())
+g = gcd(gcd(A, B), C)
 print(A // g + B // g + C // g - 3)
