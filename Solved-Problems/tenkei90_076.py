b31368b14598d3f3c5b959c0ab604b29db0a7890
es1aT <136983924+es1aT@users.noreply.github.com>
2024-08-28 21:09:53 +0900

    Update tenkei90_076.py

--- Solved-Problems/tenkei90_076.py
+++ Solved-Problems/tenkei90_076.py
@@ -1,21 +1,19 @@
 # 円環を倍にして結合し、しゃくとり法で全体の大きさの10分の1になるものを探す。
 N = int(input())
 A = list(map(int, input().split()))
-yaa = sum(A) / 10
-if yaa % 1 != 0:
+want = sum(A) / 10
+if want % 1 != 0:
   print("No")
   exit()
 A += A
 
 head, tail, cnt = 0, 0, 0
-while head < N*2 and tail < N*2:
-  if cnt > yaa:
+for head in range(N*2):
+  cnt += A[head]
+  while cnt > want:
     cnt -= A[tail]
     tail += 1
-  elif cnt < yaa:
-    cnt += A[head]
-    head += 1
-  else:
+  if cnt == want:
     print("Yes")
     exit()
 print("No")
