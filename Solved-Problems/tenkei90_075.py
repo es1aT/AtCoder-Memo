59996d05561626d1713c65ab66c11d8cf31a2c69
es1aT <136983924+es1aT@users.noreply.github.com>
2024-08-28 17:44:02 +0900

    Create tenkei90_075.py

---
+++ Solved-Problems/tenkei90_075.py
@@ -0,0 +1,24 @@
+# Nを素因数分解して、何回分割できるか計算する。
+N = int(input())
+li = []
+while N % 2 == 0:
+  li.append(2)
+  N //= 2
+f = 3
+while f * f <= N:
+    if N % f == 0:
+        li.append(f)
+        N //= f
+    else:
+        f += 2
+if N != 1:
+    li.append(N)
+
+if len(li) == 1:
+  print(0)
+else:
+  cnt, n = 0, len(li)
+  while n > 1:
+    n = (n + 1) // 2
+    cnt += 1
+  print(cnt)
