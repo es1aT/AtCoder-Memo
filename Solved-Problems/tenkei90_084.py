6a95ead25abbd54eaffa612d9ab61e86a470563e
es1aT <136983924+es1aT@users.noreply.github.com>
2024-08-29 13:56:10 +0900

    Update tenkei90_084.py

--- Solved-Problems/tenkei90_084.py
+++ Solved-Problems/tenkei90_084.py
@@ -1,6 +1,27 @@
+# ランレングス圧縮を行うことで、O(N)で解ける。
+def сжатие():
+  li = []
+  cnt = 1
+  for i in range(1, N):
+    if S[i] == S[i-1]:
+      cnt += 1
+    else:
+      li.append(cnt)
+      cnt = 1
+  li.append(cnt)
+  return li
 
+N = int(input())
+S = list(input())
+li = сжатие()
 
-# しゃくとり法でも解ける。
+su, ans = sum(li), 0
+for i in li:
+  su -= i
+  ans += su * i
+print(ans)
+
+# しゃくとり法でも解けた。
 N = int(input())
 S = list(input())
 tail = ans = 0
