4783f6dc87a83e3cc95f7561d925ddd27ee0808e
es1aT <136983924+es1aT@users.noreply.github.com>
2024-08-29 22:37:02 +0900

    Update abc368_a.py

--- Solved-Problems/abc368_a.py
+++ Solved-Problems/abc368_a.py
@@ -1,6 +1,12 @@
-# スライスの基本構文を用いる。
+# スライスの基本構文を使って処理を行う。
+
+# 入力値を取得
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
+
+# リストAをK回右に回転させる
 A = A[-K:] + A[:-K]
+
+# 結果を出力
 for i in A:
   print(i, end=" ")
