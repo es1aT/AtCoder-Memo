7c29b911f67d06cd1e7d25bd06bc8ae3554ba10f
es1aT <136983924+es1aT@users.noreply.github.com>
2024-08-29 22:54:20 +0900

    Update tenkei90_004.py

--- Solved-Problems/tenkei90_004.py
+++ Solved-Problems/tenkei90_004.py
@@ -1,24 +1,28 @@
 # 各行と各列の和を先に計算し、包除原理を用いる。
 
+# 入力値を取得
 H, W = map(int, input().split())
 A = []
-B = [[0]*W for _ in range(H)]
-I = [0]*H
-J = [0]*W
 for i in range(H):
   A.append(list(map(int, input().split())))
 
-for i in range(H):
-  for j in range(W):
-    I[i] += A[i][j]
-for j in range(W):
-  for i in range(H):
-    J[j] += A[i][j]
+# 各行と各列の和を格納するリストの初期化
+row_sums = [0] * H
+col_sums = [0] * W
 
-cnt = 0
+# 各行と各列の和を計算
 for i in range(H):
   for j in range(W):
-    B[i][j] = I[i] + J[j] - A[i][j]
+    row_sums[i] += A[i][j]
+    col_sums[j] += A[i][j]
+
+#新しい二次元配列Bに合計した値を格納
+B = [[0]*W for _ in range(H)]
+for i in range(H):
+  for j in range(W):
+    B[i][j] = row_sums[i] + col_sums[j] - A[i][j]
+
+# 二次元配列Bを出力
 for i in range(H):
   for j in range(W):
     print(B[i][j], end=" ")
