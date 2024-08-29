b8078e815246b32845481cc77730f072c5d9fe1a
es1aT <136983924+es1aT@users.noreply.github.com>
2024-08-29 22:38:40 +0900

    Update arc017_a.py

--- Solved-Problems/arc017_a.py
+++ Solved-Problems/arc017_a.py
@@ -1,6 +1,11 @@
-N = int(input()) #典型的な素数の問題
+# 典型的な素数の問題
+
+# 入力値を取得
+N = int(input())
+
+# 素数判定
 for i in range(2, int(N**0.5)+1):
   if N % i == 0:
-    print("NO")
+    print("NO") # Nが素数でない場合
     exit()
-print("YES")
+print("YES") # Nが素数である場合
