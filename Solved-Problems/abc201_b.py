af0e9d5ca71d42c5511f0a6751ed6ff26141ecb5
es1aT <136983924+es1aT@users.noreply.github.com>
2024-09-03 21:04:16 +0900

    Update abc201_b.py

--- Solved-Problems/abc201_b.py
+++ Solved-Problems/abc201_b.py
@@ -1,8 +1,10 @@
-# 二次元配列は一つ目の値を基準に、ソートを行える。
+"""
+二次元配列は一つ目の値を基準に、ソートを行える。
+"""
 N = int(input())
 li = []
 for i in range(N):
   S, T = input().split()
   li.append([int(T), S])
-li = sorted(li, reverse=True)
+li = sorted(li, reverse=True) # li = sorted(li, key=lambda x: x[a]) を用いることでNつ目の値を基準にソート可能。
 print(li[1][1])
