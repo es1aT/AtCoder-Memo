cf848d250f0a28ee6cb6103e9102be8d49eebd73
es1aT <136983924+es1aT@users.noreply.github.com>
2024-09-10 05:35:41 +0900

    Update dp_a.py

--- Solved-Problems/dp_a.py
+++ Solved-Problems/dp_a.py
@@ -6,11 +6,18 @@
 （４）答え(dp[N-1])を出力する。
 """
 
+# (1)
 N = int(input())
 h = list(map(int, input().split()))
 dp = [0]*N
+
+# (2)
 dp[0] = 0
 dp[1] = abs(h[0]-h[1])
+
+# (3)
 for i in range(2, N):
   dp[i] = min(dp[i-2] + abs(h[i-2]-h[i]), dp[i-1] + abs(h[i-1]-h[i]))
+
+# (4)
 print(dp[-1])
