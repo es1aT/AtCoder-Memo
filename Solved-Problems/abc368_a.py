053a841ce9418516f01e42df918c4d1bf07cd7b4
es1aT <136983924+es1aT@users.noreply.github.com>
2024-09-11 20:24:02 +0900

    Update abc368_a.py

--- Solved-Problems/abc368_a.py
+++ Solved-Problems/abc368_a.py
@@ -1,12 +1,18 @@
-# スライスの基本構文を使って処理を行う。
-
-# 入力値を取得
+"""
+（１）スライスの基本構文を使って処理を行う。
+（２）collectionモジュールのdeque型にあるローテート機能を用いる。
+"""
+# (1)
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
-
-# リストAをK回右に回転させる
 A = A[-K:] + A[:-K]
+for i in A:
+  print(i, end=" ")
 
-# 結果を出力
+# (2)
+from collections import deque
+N, K = map(int, input().split())
+A = deque(map(int, input().split()))
+A.rotate(K)
 for i in A:
   print(i, end=" ")
