29e1522d9443ac0c41c1d469e792d83f6f6daafc
es1aT <136983924+es1aT@users.noreply.github.com>
2024-09-16 02:16:11 +0900

    Update tenkei90_007.py

--- Solved-Problems/tenkei90_007.py
+++ Solved-Problems/tenkei90_007.py
@@ -1,5 +1,10 @@
-# 二分探索して、生徒のレーティングに最も近いクラスを探す。
+"""
+生徒のレーティングに最も近いクラスを探す方法には以下の二種類がある。
+(1) 二分探索のアルゴリズムを作成する。
+(2) bisectモジュールを用いる。
+"""
 
+# (1)
 # 入力値を取得
 N = int(input())
 A = sorted(list(map(int, input().split())))
@@ -23,3 +28,24 @@ for i in range(Q):
       print(abs(A[0] - B))
   else:
       print(min(abs(A[left] - B), abs(A[left-1] - B)))
+
+# (2)
+import bisect
+
+# 入力値を取得
+N = int(input())
+A = sorted(list(map(int, input().split())))
+Q = int(input())
+
+# 各生徒のレーティングに対して最も近いクラスを探す
+for i in range(Q):
+    B = int(input())
+    pos = bisect.bisect_left(A, B)
+    
+    # 最も近いクラスのレーティングを出力
+    if pos == N:
+        print(abs(A[N-1] - B))
+    elif pos == 0:
+        print(abs(A[0] - B))
+    else:
+        print(min(abs(A[pos] - B), abs(A[pos-1] - B)))
